3149a8be759ee286521f8f3bb32c712245c4e9b4
Patrick_Donghwi_Park <gptnc5100@gmail.com>
2024-11-01 20:07:09 +0100

    Adding multiple ShellContent elements

--- DevsPark/MauiProgram.cs
+++ DevsPark/MauiProgram.cs
@@ -1,4 +1,9 @@
 ﻿using DevsPark.MVVM.Pages.DesertMirage.NightAdventures;
+using DevsPark.MVVM.Pages.DesertMirage.OasisFun;
+using DevsPark.MVVM.Pages.Frozen.IcyTreats;
+using DevsPark.MVVM.Pages.Frozen.SnowActivities;
+using DevsPark.MVVM.Pages.TropicalParadise.BeachAttractions;
+using DevsPark.MVVM.Pages.TropicalParadise.JungleAdventures;
 using DevsPark.MVVM.ViewModels;
 using Microsoft.Extensions.Logging;
 
@@ -25,7 +30,17 @@ namespace DevsPark
 #endif
 
             builder.Services.AddTransient<NightDuneBuggy>();
-
+            builder.Services.AddTransient<StarGazing>();
+            builder.Services.AddTransient<CamelRides>();
+            builder.Services.AddTransient<OasisPools>();
+            builder.Services.AddTransient<FrozenDrinksBar>();
+            builder.Services.AddTransient<IceCreamWorld>();
+            builder.Services.AddTransient<IceSculptures>();
+            builder.Services.AddTransient<Snowboarding>();
+            builder.Services.AddTransient<SurfLessons>();
+            builder.Services.AddTransient<WavePool>();
+            builder.Services.AddTransient<AnimalShows>();
+            builder.Services.AddTransient<ZipLines>();
             builder.Services.AddTransient<PlacesViewModel>();
             
             return builder.Build();
