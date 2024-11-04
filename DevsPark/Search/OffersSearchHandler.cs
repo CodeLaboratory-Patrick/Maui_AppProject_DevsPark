9240f5de6447d142647245bc268b3082195db2da
Patrick_Donghwi_Park <gptnc5100@gmail.com>
2024-11-04 11:47:00 +0100

    Filtering results in the Search Handler (<search:OffersSearchHandler>, OnQueryChanged())

--- DevsPark/Search/OffersSearchHandler.cs
+++ DevsPark/Search/OffersSearchHandler.cs
@@ -3,19 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevsPark.MVVM.Models;
 
 namespace DevsPark.Search
 {
     public class OffersSearchHandler : SearchHandler
     {
+        public List<Offer>  Suggestions { get; set; } = new List<Offer>();
         public OffersSearchHandler()
         {
             var service = Application.Current.MainPage
                 .Handler
                 .MauiContext
                 .Services.GetService<OffersService>();
+            
+            Suggestions = service.Offers;
 
-            ItemsSource = service.Offers;
+            ItemsSource = Suggestions;
+        }
+        
+        protected override void OnQueryChanged(string oldValue, string newValue)
+        {
+            if(string.IsNullOrEmpty(newValue))
+            {
+                ItemsSource = Suggestions;
+            }
+            else
+            {
+                ItemsSource = Suggestions
+                    .Where(offer => offer.OfferName.ToLower().Contains(newValue.ToLower()))
+                    .ToList();
+            }
         }
     }
 }
